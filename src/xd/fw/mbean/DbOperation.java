573a099a37a631a1e71879a8a2106eb1db7640f6
Xiaodong LV <xiaodong.lv@ericsson.com>
2016-06-17 16:19:57 +0800

    run script file in mbean

--- src/xd/fw/mbean/DbOperation.java
+++ src/xd/fw/mbean/DbOperation.java
@@ -1,5 +1,8 @@
 package xd.fw.mbean;
 
+import org.apache.derby.tools.ij;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jmx.export.annotation.ManagedOperation;
@@ -7,12 +10,17 @@ import org.springframework.jmx.export.annotation.ManagedOperationParameter;
 import org.springframework.jmx.export.annotation.ManagedOperationParameters;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Service;
+import xd.fw.FwUtil;
 
-import java.lang.reflect.Method;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Service
 @ManagedResource(objectName = "xapp:name=DbOperation", description = "derby db operations")
 public class DbOperation {
+    Logger logger = LoggerFactory.getLogger(DbOperation.class);
+
     @Autowired
     JdbcTemplate derbyTpl;
 
@@ -22,6 +30,9 @@ public class DbOperation {
             @ManagedOperationParameter(name="rowNumber",description = "rowNumber")
     })
     public String selectSql(String sql, int rowNumber){
+        if (!sql.toLowerCase().startsWith("select")){
+            return "invalidate sql";
+        }
         StringBuffer buffer = new StringBuffer();
         derbyTpl.query(sql,(rs)->{
             for (int i=0;i<rowNumber;i++){
@@ -40,5 +51,23 @@ public class DbOperation {
         derbyTpl.execute(sql);
         return "OK";
     }
+
+    @ManagedOperation(description = "Run File")
+    @ManagedOperationParameters({
+            @ManagedOperationParameter(name="file",description = "file")
+    })
+    public String runFile(String file){
+        File scriptFile = new File(file);
+        if (!scriptFile.exists()){
+            return "file not exists";
+        }
+        try(InputStream inputStream = new FileInputStream(scriptFile)){
+            ij.runScript(derbyTpl.getDataSource().getConnection(), inputStream, FwUtil.UTF8, System.out, FwUtil.UTF8);
+        } catch (Exception e){
+            logger.error("fail to run script: " + file , e);
+        }
+        return "OK";
+    }
+
 
 }
