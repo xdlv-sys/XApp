18a0ddb2698daae5bf6fd83831d099790c9ff63e
xdlv <kkjattuu@163.com>
2016-07-07 11:08:53 +0800

    add parameters info

--- src/xd/fw/SessionFilter.java
+++ src/xd/fw/SessionFilter.java
@@ -1,5 +1,6 @@
 package xd.fw;
 
+import org.apache.log4j.Logger;
 import xd.fw.FwUtil;
 import xd.fw.action.BaseAction;
 
@@ -7,12 +8,19 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
 
 public class SessionFilter implements Filter {
 
+    static Logger logger = Logger.getLogger(SessionFilter.class);
+
     String[] whiteNames;
+    boolean showParameters = false;
     public void init(FilterConfig filterconfig) throws ServletException {
         whiteNames = filterconfig.getInitParameter("whiteNames").split(",");
+        showParameters = Boolean.valueOf(filterconfig.getInitParameter("showParameters"));
     }
 
     boolean accept(String uri){
@@ -36,6 +44,22 @@ public class SessionFilter implements Filter {
             response.setHeader("Prama", "no-cache");
             response.sendRedirect("result.cmd?needLogin=true");
             return;
+        }
+        if (showParameters){
+            StringBuilder builder = new StringBuilder(uri).append(":\n");
+            Map parameterMap = servletrequest.getParameterMap();
+            for (Iterator iter = parameterMap.keySet().iterator(); iter.hasNext();) {
+                String name = (String) iter.next();
+                builder.append(name).append("=");
+                Object value = parameterMap.get(name);
+                if (value instanceof String[]){
+                    builder.append(Arrays.toString((String[])value));
+                } else {
+                    builder.append(value);
+                }
+                builder.append(",");
+            }
+            logger.info(builder.toString());
         }
         filterchain.doFilter(servletrequest, resp);
     }
