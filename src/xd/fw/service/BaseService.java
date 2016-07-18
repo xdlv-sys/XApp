240a659f23dbd887fddc303993ed9a53e28d59c6
Xiaodong LV <xiaodong.lv@ericsson.com>
2016-07-18 10:30:05 +0800

    add hibernate query common

--- src/xd/fw/service/BaseService.java
+++ src/xd/fw/service/BaseService.java
@@ -1,10 +1,31 @@
 package xd.fw.service;
 
+import java.io.Serializable;
+import java.util.List;
+
 public interface BaseService {
 
-	int getAllCount();
-	
+    int getAllCount();
+
+	int getAllCount(Class<?> cls);
+
+    <T> List<T> getList(Class<T> cls, String orderBy, int start, int limit);
+
 	int getPrimaryKey(String tableName);
 
 	void saveOrUpdate(Object entity);
+
+	<T> T get(Class<T> tClass,Serializable id);
+
+	//<T> T load(Class<T> tClass,Serializable id);
+
+	void update(Object entity);
+
+	void save(Object entity);
+
+	void merge(Object entity);
+
+	void delete (Object entity);
+
+    <T> List<T> getList(String hsql);
 }
--- src/xd/fw/service/impl/BaseServiceImpl.java
+++ src/xd/fw/service/impl/BaseServiceImpl.java
@@ -7,48 +7,91 @@ import xd.fw.bean.PrimaryKey;
 import xd.fw.bean.mapper.PrimaryKeyMapper;
 import xd.fw.service.BaseService;
 
+import java.io.Serializable;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 public class BaseServiceImpl implements BaseService {
 
-	@Autowired
+    @Autowired
     PrimaryKeyMapper primaryKeyMapper;
-	
-	static Map<String, int[]> primaryKeyMap = new HashMap<String, int[]>();
-	final static int PRE_OCCUPY_MAX = 10;
-	
-	public void setPrimaryKeyMapper(PrimaryKeyMapper primaryKeyMapper) {
-		this.primaryKeyMapper = primaryKeyMapper;
-	}
-	
-	@Override
-	public int getAllCount() {
-		return 0;
-	}
 
-	@Override
-	public int getPrimaryKey(String tableName) {
-		int[] value;
-		synchronized (primaryKeyMap) {
-			value = primaryKeyMap.get(tableName);
-			if (value == null || value[0] == value[1]){
-				PrimaryKey primaryKey = primaryKeyMapper.selectByPrimaryKey(tableName);
-				if (primaryKey == null){
-					value = new int[]{1, PRE_OCCUPY_MAX};
-					primaryKeyMapper.insert(new PrimaryKey(tableName, PRE_OCCUPY_MAX));
-				} else {
-					value = new int[]{primaryKey.getCurrentId() + 1, primaryKey.getCurrentId() + PRE_OCCUPY_MAX};
-					primaryKeyMapper.updateByPrimaryKey(new PrimaryKey(tableName, value[1]));
-				}
-				primaryKeyMap.put(tableName, value);
-			} else {
-				value[0] += 1;
-			}
-		}
-		return value[0];
-	}
+    static Map<String, int[]> primaryKeyMap = new HashMap<String, int[]>();
+    final static int PRE_OCCUPY_MAX = 10;
 
-	public void saveOrUpdate(Object entity){
-		throw new RuntimeException("no implementation");
-	}
+    public void setPrimaryKeyMapper(PrimaryKeyMapper primaryKeyMapper) {
+        this.primaryKeyMapper = primaryKeyMapper;
+    }
+
+    @Override
+    public int getAllCount() {
+        return 0;
+    }
+
+    @Override
+    public int getPrimaryKey(String tableName) {
+        int[] value;
+        synchronized (primaryKeyMap) {
+            value = primaryKeyMap.get(tableName);
+            if (value == null || value[0] == value[1]) {
+                PrimaryKey primaryKey = primaryKeyMapper.selectByPrimaryKey(tableName);
+                if (primaryKey == null) {
+                    value = new int[]{1, PRE_OCCUPY_MAX};
+                    primaryKeyMapper.insert(new PrimaryKey(tableName, PRE_OCCUPY_MAX));
+                } else {
+                    value = new int[]{primaryKey.getCurrentId() + 1, primaryKey.getCurrentId() + PRE_OCCUPY_MAX};
+                    primaryKeyMapper.updateByPrimaryKey(new PrimaryKey(tableName, value[1]));
+                }
+                primaryKeyMap.put(tableName, value);
+            } else {
+                value[0] += 1;
+            }
+        }
+        return value[0];
+    }
+
+    public void saveOrUpdate(Object entity) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public int getAllCount(Class<?> cls) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public <T> List<T> getList(Class<T> cls, String orderBy, int start, int limit) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public <T> T get(Class<T> tClass, Serializable id) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public void update(Object entity) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public void save(Object entity) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public void merge(Object entity) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public void delete(Object entity) {
+        throw new RuntimeException("no implementation");
+    }
+
+    @Override
+    public <T> List<T> getList(String hsql) {
+        throw new RuntimeException("no implementation");
+    }
 }
