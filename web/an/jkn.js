7280180a6d35b198e97cb870f4eb87b4511ca4a5
xdlv <kkjattuu@163.com>
2016-09-19 12:08:03 +0800

    format

--- web/an/jkn.js
+++ web/an/jkn.js
@@ -1,36 +1,33 @@
-var app = angular.module("interfaceTest", ['angular-md5']
-).config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.defaults.transformRequest = function (obj) {
-            var str = [];
-            for (var p in obj) {
-                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            }
-            return str.join("&");
+var app = angular.module("interfaceTest", ['angular-md5']).config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.transformRequest = function(obj) {
+        var str = [];
+        for (var p in obj) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
         }
-        $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-        $httpProvider.defaults.headers.post = {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
-        };
-    }]
-).controller('interfaceCtrl', ['$scope', 'md5', function ($scope, md5) {
-        //$scope.signKey = 'jkn@igecono.com0516';
-    }]
-).directive("inputForm", ['$http', 'md5', '$rootScope', function ($http, md5, $rootScope) {
+        return str.join("&");
+    }
+    $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+    $httpProvider.defaults.headers.post = {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+    };
+}]).controller('interfaceCtrl', ['$scope', 'md5', function($scope, md5) {
+    //$scope.signKey = 'jkn@igecono.com0516';
+}]).directive("inputForm", ['$http', 'md5', '$rootScope', function($http, md5, $rootScope) {
     return {
         templateUrl: 'inputForm.html',
         replace: true,
         scope: {
             //signKey: 'jkn@igecono.com0516'
         },
-        link: function (scope, element, attrs) {
+        link: function(scope, element, attrs) {
             scope.signKey = 'jkn@igecono.com0516';
             scope.inputs = [];
             scope.conf = [];
             scope.resultKey = attrs.result;
 
-            scope.getHttpParams = function () {
+            scope.getHttpParams = function() {
                 var httpParams = {};
-                angular.forEach(scope.inputs, function (v, i, a) {
+                angular.forEach(scope.inputs, function(v, i, a) {
                     if (scope.conf[i]) {
                         httpParams[v.name] = scope.conf[i];
                     }
@@ -38,7 +35,7 @@ var app = angular.module("interfaceTest", ['angular-md5']
                 return httpParams;
             };
 
-            scope.change = function () {
+            scope.change = function() {
                 var httpParams = scope.getHttpParams();
                 var str = '';
                 var keys = [];
@@ -48,24 +45,24 @@ var app = angular.module("interfaceTest", ['angular-md5']
                     }
                 }
                 keys = keys.sort();
-                angular.forEach(keys, function (v, i) {
+                angular.forEach(keys, function(v, i) {
                     str += (v + '=' + httpParams[v] + '&');
                 });
 
                 str += 'key=' + scope.signKey;
                 scope.currentSign = str;
-                angular.forEach(scope.inputs, function (v, i, a) {
+                angular.forEach(scope.inputs, function(v, i, a) {
                     if (v.name === 'sign') {
                         scope.conf[i] = md5.createHash(str);
                     }
                 });
             };
 
-            scope.submitForm = function () {
+            scope.submitForm = function() {
                 var httpParams = scope.getHttpParams();
 
-                $http.post(scope.action, httpParams).success(function (data) {
-                    if (attrs.result){
+                $http.post(scope.action, httpParams).success(function(data) {
+                    if (attrs.result) {
                         scope.result = data[scope.resultKey];
                     }
 
---
+++ web/wwt/wxh5.jsp
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+    <%@ include file="js.html"%>    
+    <script type="text/javascript">
+        var app = angular.module("wxh5", ['ui.bootstrap']);
+        app.controller('wxh5Ctrl', ['$scope','common', function($scope,common){
+            common.post('pay!wxPay.cmd', {
+                'carParkInfo.parkId':'111',
+                'watchId':'0001',
+                'carParkInfo.carNumber':'苏A',
+                'carType':0
+            }, function(data) {
+                var wxOrder = data.wxOrder;
+                if (common.isBlank(wxOrder)) {
+                    common.error('无法完成支付，请重试');
+                    return;
+                }
+                var tmp = '';
+                angular.forEach(wxOrder, function(v, i){
+                    tmp += ('&' + i + '=' + encodeURI(v));
+                });
+                $scope.clickAction = 'weixin://wap/pay?' + encodeURI(tmp.substring(1));
+            });
+        }]);
+    </script>
+    <script src="common.js"></script>
+    <style type="text/css">
+    .center-modal {
+        position: fixed;
+        top: 30%;
+    }
+    </style>
+</head>
+
+<body ng-app="wxh5" ng-controller="wxh5Ctrl">
+    <input type="text" ng-model="clickAction">
+    <a ng-href="clickAction">Pay</a>
+</body>
+</html>
