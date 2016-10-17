5e67ecb16d41bfd204bbefd1f8498eda97b4cd2e
xdlv <kkjattuu@163.com>
2016-10-17 19:43:51 +0800

    remote imports

--- src/xd/fw/mina/tlv/ReversedProxy.java
+++ src/xd/fw/mina/tlv/ReversedProxy.java
@@ -3,13 +3,9 @@ package xd.fw.mina.tlv;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.mina.core.future.ConnectFuture;
-import org.apache.mina.core.service.IoProcessor;
-import org.apache.mina.core.service.SimpleIoProcessorPool;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.SocketConnector;
-import org.apache.mina.transport.socket.nio.NioProcessor;
-import org.apache.mina.transport.socket.nio.NioSession;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,13 +16,9 @@ import org.springframework.stereotype.Service;
 import xd.fw.FwUtil;
 import xd.fw.I18n;
 import xd.fw.job.BaseJob;
-import xd.fw.mina.ParkProxy;
 
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
