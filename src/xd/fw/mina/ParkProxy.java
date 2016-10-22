da41fd3503a754c7c867648a7f6a8ef90da80d23
xdlv <kkjattuu@163.com>
2016-10-22 16:45:53 +0800

    v1.1

--- src/xd/fw/mina/ParkProxy.java
+++ src/xd/fw/mina/ParkProxy.java
@@ -1,8 +1,6 @@
 package xd.fw.mina;
 
 import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
