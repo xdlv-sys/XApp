b0a0409a895c6e5a9b1ff85a13c324aeaef5ba20
xdlv <kkjattuu@163.com>
2016-10-27 15:59:56 +0800

    v1.2

--- src/xd/fw/scheduler/UpgradeProxyListener.java
+++ src/xd/fw/scheduler/UpgradeProxyListener.java
@@ -3,6 +3,7 @@ package xd.fw.scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import xd.fw.mina.tlv.ReversedHandler;
 @Async
 public class UpgradeProxyListener implements ApplicationListener<UpgradeProxyEvent> {
     static Logger logger = LoggerFactory.getLogger(UpgradeProxyListener.class);
+    @Qualifier("parkHandler")
     @Autowired
     ReversedHandler reversedHandler;
 
