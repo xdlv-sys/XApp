bb0fbcee0c00ccce4e7bcbb3fdf30c593b94937e
xdlv <kkjattuu@163.com>
2017-05-22 17:15:43 +0800

    entering place

--- src/xd/dl/mina/EnterProcess2.java
+++ src/xd/dl/mina/EnterProcess2.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import xd.fw.mina.tlv.TLVMessage;
 
+import java.text.SimpleDateFormat;
+
 /**
  * 上报云平台，用于推送消息
  */
@@ -21,7 +23,10 @@ public class EnterProcess2 extends EnterProcess {
         return new String[][]{
                 {"parkId", parkId},
                 {"carNumber", carNumber},
-                {"enterTime", enterTime}
+                //2017-05-22 164708
+                {"enterTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(
+                        new SimpleDateFormat("yyyyMMddHHmmss").parse(enterTime)
+                )}
         };
     }
 
